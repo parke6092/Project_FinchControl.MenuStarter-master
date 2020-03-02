9714ebe4aeb80769ed5d91d26c6a20e5796378f6
Drake Parker <parke251@ad.nmc.edu>
2020-03-02 15:02:29 -0500

    Add project files.

---
+++ Project_FinchControl/Program.cs
@@ -0,0 +1,524 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FinchAPI;
+
+namespace Project_FinchControl
+{
+
+    // **************************************************
+    //
+    // Title: Finch Control - Menu Starter
+    // Description: Starter solution with the helper methods,
+    //              opening and closing screens, and the menu
+    // Application Type: Console
+    // Author: Velis, John
+    // Dated Created: 1/22/2020
+    // Last Modified: 1/25/2020
+    //
+    // **************************************************
+
+    class Program
+    {
+        /// <summary>
+        /// first method run when the app starts up
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            SetTheme();
+
+            DisplayWelcomeScreen();
+            DisplayMenuScreen();
+            DisplayClosingScreen();
+        }
+
+        /// <summary>
+        /// setup the console theme
+        /// </summary>
+        static void SetTheme()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Main Menu                                 *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayMenuScreen()
+        {
+            Console.CursorVisible = true;
+
+            bool quitApplication = false;
+            string menuChoice;
+
+            Finch finchRobot = new Finch();
+
+            do
+            {
+                DisplayScreenHeader("Main Menu");
+
+                //
+                // get user menu choice
+                //
+                Console.WriteLine("\ta) Connect Finch Robot");
+                Console.WriteLine("\tb) Talent Show");
+                Console.WriteLine("\tc) Data Recorder");
+                Console.WriteLine("\td) Alarm System");
+                Console.WriteLine("\te) User Programming");
+                Console.WriteLine("\tf) Disconnect Finch Robot");
+                Console.WriteLine("\tq) Quit");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                //
+                // process user menu choice
+                //
+                switch (menuChoice)
+                {
+                    case "a":
+                        DisplayConnectFinchRobot(finchRobot);
+                        break;
+
+                    case "b":
+                        DisplayTalentShowMenuScreen(finchRobot);
+                        break;
+
+                    case "c":
+                        LightAlarmDisplayMenuScreen(finchRobot);
+                        break;
+
+                    case "d":
+
+                        break;
+
+                    case "e":
+
+                        break;
+
+                    case "f":
+                        DisplayDisconnectFinchRobot(finchRobot);
+                        break;
+
+                    case "q":
+                        DisplayDisconnectFinchRobot(finchRobot);
+                        quitApplication = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitApplication);
+
+        }
+
+        #region ALARM SYSTEM
+
+        static void LightAlarmDisplayMenuScreen(Finch finchRobot)
+        {
+            Console.CursorVisible = true;
+
+            string sensorsToMonitor = "";
+            string rangeType = "";
+
+            int minMaxThresholdValue = 0;
+            int timeToMonitor = 0;
+
+            bool quitMenu = false;
+            string menuChoice;
+
+            do
+            {
+                DisplayScreenHeader("Light Alarm Menu");
+
+                //
+                // get user menu choice
+                //
+                Console.WriteLine("\ta) Set Sensors to Monitor");
+                Console.WriteLine("\tb) Set Range Type");
+                Console.WriteLine("\tc) Set Maximum/Minimum Threshold Value");
+                Console.WriteLine("\td) Set Time to Monitor");
+                Console.WriteLine("\te) Set Alarm");
+                Console.WriteLine("\tq) Main Menu");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                //
+                // process user menu choice
+                //
+                switch (menuChoice)
+                {
+                    case "a":
+                        sensorsToMonitor = LightAlarmDisplaySetSensersToMonitor();
+                        break;
+
+                    case "b":
+                        LightAlarmDisplaySetRangeType();
+                        break;
+
+                    case "c":
+                        minMaxThresholdValue = LightAlarmDisplaySetMinMaxThresholdValue(rangeType, finchRobot);
+                        break;
+
+                    case "d":
+                        timeToMonitor = LightAlarmDisplaySetMaximumTimeToMonitor(timeToMonitor, finchRobot);
+                        break;
+
+                    case "e":
+
+                        break;
+
+                    case "q":
+                        quitMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitMenu);
+
+        }
+
+        static string LightAlarmDisplaySetSensersToMonitor()
+        {
+            string sensorsToMonitor;
+
+            DisplayScreenHeader("Sensors to Monitor");
+
+            Console.Write("Sensors to Monitor");
+            sensorsToMonitor = Console.ReadLine();
+
+
+            DisplayContinuePrompt();
+
+            return sensorsToMonitor;
+        }
+
+        static string LightAlarmDisplaySetRangeType()
+        {
+            string rangeType;
+
+            DisplayScreenHeader("Range Type");
+
+            Console.Write("Range Type");
+            rangeType = Console.ReadLine();
+
+
+            DisplayContinuePrompt();
+
+            return rangeType;
+        }
+
+        static int LightAlarmDisplaySetMinMaxThresholdValue(string  rangeType, Finch finchRobot)
+        {
+            int minMaxThresholdValue;
+
+            DisplayScreenHeader("Min/Max Threshold Value");
+
+            Console.WriteLine($"Current left light sensor value: {finchRobot.getLeftLightSensor()}");
+            Console.WriteLine($"Current right light sensor value: {finchRobot.getRightLightSensor()}");
+            Console.WriteLine();
+
+            Console.Write($"{rangeType} light sensor value: ");
+            int.TryParse(Console.ReadLine(), out minMaxThresholdValue);
+
+            // validation
+            bool validResponse;
+
+            do
+            {
+
+                DisplayScreenHeader("Min/Max Threshold Value");
+
+                Console.WriteLine($"Current left light sensor value: {finchRobot.getLeftLightSensor()}");
+                Console.WriteLine($"Current right light sensor value: {finchRobot.getRightLightSensor()}");
+                Console.WriteLine();
+
+                Console.Write($"{rangeType} light sensor value: ");
+                validResponse = int.TryParse(Console.ReadLine(), out minMaxThresholdValue);
+
+
+                if (!validResponse)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please Enter an integer.");
+                }
+
+                DisplayContinuePrompt();
+            } while (!validResponse);
+
+            // echo back
+
+            return minMaxThresholdValue;
+        }
+        static int LightAlarmDisplaySetMaximumTimeToMonitor(int timeToMonitor, Finch finchRobot)
+        {
+            int maxSecondsToMonitor;
+
+            DisplayScreenHeader("Time to Monitor");
+
+           
+            Console.Write($"[seconds]");
+            int.TryParse(Console.ReadLine(), out maxSecondsToMonitor);
+
+            // validation
+            bool validResponse;
+
+            do
+            {
+
+                DisplayScreenHeader("Min/Max Threshold Value");
+
+                Console.WriteLine($"Current left light sensor value: {finchRobot.getLeftLightSensor()}");
+                Console.WriteLine($"Current right light sensor value: {finchRobot.getRightLightSensor()}");
+                Console.WriteLine();
+
+                Console.Write($"{timeToMonitor} light sensor value: ");
+                validResponse = int.TryParse(Console.ReadLine(), out maxSecondsToMonitor);
+
+
+                if (!validResponse)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please Enter an integer.");
+                }
+
+                DisplayContinuePrompt();
+            } while (!validResponse);
+
+            // echo back
+
+            return maxSecondsToMonitor;
+        }
+
+        #endregion
+
+        #region TALENT SHOW
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Talent Show Menu                          *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayTalentShowMenuScreen(Finch myFinch)
+        {
+            Console.CursorVisible = true;
+
+            bool quitTalentShowMenu = false;
+            string menuChoice;
+
+            do
+            {
+                DisplayScreenHeader("Talent Show Menu");
+
+                //
+                // get user menu choice
+                //
+                Console.WriteLine("\ta) Light and Sound");
+                Console.WriteLine("\tb) ");
+                Console.WriteLine("\tc) ");
+                Console.WriteLine("\td) ");
+                Console.WriteLine("\tq) Main Menu");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                //
+                // process user menu choice
+                //
+                switch (menuChoice)
+                {
+                    case "a":
+                        DisplayLightAndSound(myFinch);
+                        break;
+
+                    case "b":
+
+                        break;
+
+                    case "c":
+
+                        break;
+
+                    case "d":
+
+                        break;
+
+                    case "q":
+                        quitTalentShowMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitTalentShowMenu);
+        }
+
+        /// <summary>
+        /// *****************************************************************
+        /// *               Talent Show > Light and Sound                   *
+        /// *****************************************************************
+        /// </summary>
+        /// <param name="finchRobot">finch robot object</param>
+        static void DisplayLightAndSound(Finch finchRobot)
+        {
+            Console.CursorVisible = false;
+
+            DisplayScreenHeader("Light and Sound");
+
+            Console.WriteLine("\tThe Finch robot will not show off its glowing talent!");
+            DisplayContinuePrompt();
+
+            for (int lightSoundLevel = 0; lightSoundLevel < 255; lightSoundLevel++)
+            {
+                finchRobot.setLED(lightSoundLevel, lightSoundLevel, lightSoundLevel);
+                finchRobot.noteOn(lightSoundLevel * 100);
+            }
+
+            DisplayMenuPrompt("Talent Show Menu");
+        }
+
+        #endregion
+
+        #region FINCH ROBOT MANAGEMENT
+
+        /// <summary>
+        /// *****************************************************************
+        /// *               Disconnect the Finch Robot                      *
+        /// *****************************************************************
+        /// </summary>
+        /// <param name="finchRobot">finch robot object</param>
+        static void DisplayDisconnectFinchRobot(Finch finchRobot)
+        {
+            Console.CursorVisible = false;
+
+            DisplayScreenHeader("Disconnect Finch Robot");
+
+            Console.WriteLine("\tAbout to disconnect from the Finch robot.");
+            DisplayContinuePrompt();
+
+            finchRobot.disConnect();
+
+            Console.WriteLine("\tThe Finch robot is now disconnect.");
+
+            DisplayMenuPrompt("Main Menu");
+        }
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                  Connect the Finch Robot                      *
+        /// *****************************************************************
+        /// </summary>
+        /// <param name="finchRobot">finch robot object</param>
+        /// <returns>notify if the robot is connected</returns>
+        static bool DisplayConnectFinchRobot(Finch finchRobot)
+        {
+            Console.CursorVisible = false;
+
+            bool robotConnected;
+
+            DisplayScreenHeader("Connect Finch Robot");
+
+            Console.WriteLine("\tAbout to connect to Finch robot. Please be sure the USB cable is connected to the robot and computer now.");
+            DisplayContinuePrompt();
+
+            robotConnected = finchRobot.connect();
+
+            // TODO test connection and provide user feedback - text, lights, sounds
+
+            DisplayMenuPrompt("Main Menu");
+
+            //
+            // reset finch robot
+            //
+            finchRobot.setLED(0, 0, 0);
+            finchRobot.noteOff();
+
+            return robotConnected;
+        }
+
+        #endregion
+
+        #region USER INTERFACE
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Welcome Screen                            *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayWelcomeScreen()
+        {
+            Console.CursorVisible = false;
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tFinch Control");
+            Console.WriteLine();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// *****************************************************************
+        /// *                     Closing Screen                            *
+        /// *****************************************************************
+        /// </summary>
+        static void DisplayClosingScreen()
+        {
+            Console.CursorVisible = false;
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tThank you for using Finch Control!");
+            Console.WriteLine();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display continue prompt
+        /// </summary>
+        static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\tPress any key to continue.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// display menu prompt
+        /// </summary>
+        static void DisplayMenuPrompt(string menuName)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"\tPress any key to return to the {menuName} Menu.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// display screen header
+        /// </summary>
+        static void DisplayScreenHeader(string headerText)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + headerText);
+            Console.WriteLine();
+        }
+
+        #endregion
+    }
+}
